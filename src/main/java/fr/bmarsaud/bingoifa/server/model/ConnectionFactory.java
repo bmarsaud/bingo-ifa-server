4abff6e6450018d3be8df5b8fcc45d261afa8a91
TheZopo <pro@bastien-marsaud.fr>
2019-01-23 13:55:12 +0100

    Provide test database to uni-tests
    Provide test database to unit tests
    Add default database configuration
    Move target to Java 11

--- src/main/java/fr/bmarsaud/bingoifa/server/model/ConnectionFactory.java
+++ src/main/java/fr/bmarsaud/bingoifa/server/model/ConnectionFactory.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -18,9 +19,11 @@ public class ConnectionFactory {
      */
     private static void createConnection() {
         Properties properties = new Properties();
+        String propertiesFileName = "/database.properties";
+        if(BingoIFAServer.class.getResource(propertiesFileName) == null) propertiesFileName = "/default-database.properties";
 
         try {
-            properties.load(BingoIFAServer.class.getResourceAsStream("/database.properties"));
+            properties.load(BingoIFAServer.class.getResourceAsStream(propertiesFileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
