265fef65ac4df8c63abe1601963d7f776edbf0a3
TheZopo <pro@bastien-marsaud.fr>
2019-02-14 11:35:07 +0100

    Anonymize sentences' label

--- src/test/java/fr/bmarsaud/bingoifa/server/mock/SentenceMock.java
+++ src/test/java/fr/bmarsaud/bingoifa/server/mock/SentenceMock.java
@@ -1,29 +1,28 @@
 package fr.bmarsaud.bingoifa.server.mock;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import fr.bmarsaud.bingoifa.server.entity.Sentence;
 
 public class SentenceMock {
     public static List<Sentence> sentences = List.of(
-        new Sentence(1, "Bastien perd à CurveFever", 0, 0, true),
-        new Sentence(2, "Tania est déclenchée par une remarque sexiste/misogyne", 0, 0, true),
-        new Sentence(3, "Hugo fait un Sudoku", 0, 0, true),
-        new Sentence(4, "La team café arrive en retard", 0, 0, true),
-        new Sentence(5, "Valentin G pose une question que personne ne comprend", 0, 0, true),
-        new Sentence(6, "“Je le méprise”", 0, 0, true),
-        new Sentence(7, "Balthazar est insolent", 0, 0, true),
-        new Sentence(8, "Théo fait une blague bien nulle/limite", 0, 0, true),
-        new Sentence(9, "Nathan fait un référence à un vieux film", 0, 0, true),
-        new Sentence(10, "Antoine fait une référence à un humoriste belge", 0, 0, true),
-        new Sentence(11, "Nathan/Antoine/Maxime fait un accent beauf", 0, 0, true),
-        new Sentence(12, "Bastien défend Kévin, son amour", 0, 0, true),
-        new Sentence(13, "Le mot “plafond d’auto-entrepreneur”", 0, 0, true),
-        new Sentence(14, "On fait référence à la richesse excessive de Valentin ", 0, 0, true),
-        new Sentence(15, "Hugo rage", 0, 0, true),
-        new Sentence(16, "Un “BEEP” de Linux", 0, 0, true),
-        new Sentence(17, "Maxime parle de BFV", 0, 0, true)
+        new Sentence(1, "First sentence", 0, 0, true),
+        new Sentence(2, "Second sentence", 0, 0, true),
+        new Sentence(3, "Third sentence", 0, 0, true),
+        new Sentence(4, "Fourth sentence", 0, 0, true),
+        new Sentence(5, "Fifth sentence", 0, 0, true),
+        new Sentence(6, "Sixth sentence", 0, 0, true),
+        new Sentence(7, "Seventh sentence", 0, 0, true),
+        new Sentence(8, "Eighth sentence", 0, 0, true),
+        new Sentence(9, "Ninth sentence", 0, 0, true),
+        new Sentence(10, "Tenth sentence", 0, 0, true),
+        new Sentence(11, "Eleventh sentence", 0, 0, true),
+        new Sentence(12, "Twelfth sentence", 0, 0, true),
+        new Sentence(13, "Thirteenth sentence", 0, 0, true),
+        new Sentence(14, "Fourteenth sentence", 0, 0, true),
+        new Sentence(15, "Fifteenth sentence", 0, 0, true),
+        new Sentence(16, "Sixteenth sentence", 0, 0, true),
+        new Sentence(17, "Seventeenth sentence", 0, 0, true)
     );
 
     public static Sentence dummySentence = new Sentence("Dummy text", 1, 1, false);
