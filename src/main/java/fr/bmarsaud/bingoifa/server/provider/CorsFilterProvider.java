9f236ce5e08f2592c1d8318b6d6a293493489f2a
TheZopo <pro@bastien-marsaud.fr>
2019-05-23 14:02:56 +0200

    Add CORS request handling

---
+++ src/main/java/fr/bmarsaud/bingoifa/server/provider/CorsFilterProvider.java
@@ -0,0 +1,50 @@
+package fr.bmarsaud.bingoifa.server.provider;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+public class CorsFilterProvider implements ContainerRequestFilter, ContainerResponseFilter {
+    /**
+     * Method for ContainerRequestFilter.
+     */
+    @Override
+    public void filter(ContainerRequestContext request) throws IOException {
+        if (isPreflightRequest(request)) {
+            request.abortWith(Response.ok().build());
+            return;
+        }
+    }
+
+    /**
+     * A preflight request is an OPTIONS request
+     * with an Origin header.
+     */
+    private static boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null && request.getMethod().equalsIgnoreCase("OPTIONS");
+    }
+
+    /**
+     * Method for ContainerResponseFilter.
+     */
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        if (request.getHeaderString("Origin") == null) return;
+
+        if (isPreflightRequest(request)) {
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            response.getHeaders().add("Access-Control-Allow-Headers", "X-Authorization-Username, X-Authorization-Timestamp, X-Authorization-Signature");
+        }
+
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+    }
+}
